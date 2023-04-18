91e43dc8965efa3316161c75d6b48e879080ab1f
Xing Wu <xingwu.cs@gmail.com>
2023-04-17 22:20:58 -0700

    small adjustments

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -45,9 +45,9 @@ export const Chat: FC<ChatProps> = ({ initialMessages, onSend, disableSendWhileT
 
   return (
     <div className="chatlite-container flex flex-col rounded-lg sm:border border-neutral-300 h-full">
-      {title && <div className="absolute chatlite-header rounded-t-lg w-full align-middle text-center text-md">{title}</div>}
+      {title && <div className="absolute chatlite-header rounded-t-lg w-full flex items-center justify-center h-8"><p>{title}</p></div>}
       <div className="px-2 sm:p-4 flex flex-col h-full justify-between flex-grow">
-        <div className="mt-4 flex flex-col overflow-y-auto scrollable-container">
+        <div className="mt-10 sm:mt-6 flex flex-col overflow-y-auto scrollable-container">
           {messages.map((message, index) => (
             <div key={index} className="my-1 sm:my-1.5">
               <ChatMessage message={message} />
